19e21dc292430353d184130e165b9c65edd9566b
Ana Silveira <anarodri@student.42quebec.com>
2022-10-13 13:28:40 -0400

    Added macros, reviewed data structures

--- include/minishell.h
+++ include/minishell.h
@@ -16,23 +16,53 @@
 # include <errno.h>
 # include <term.h>
 
-// GLOBAL VARIABLE AND STRUCTS
-//int	g_exit_status;
+// MACROS, GLOBAL VARIABLE AND STRUCTS
+# define PIPE_READ 0
+# define PIPE_WRITE 1
+# define WHITESPACE " \n\t\v\r\f"
+# define METACHAR "|<> \n\t\v\r\f"
+
+# define ERR_SYNTAX "Syntax error near unexpected token"
+# define ERR_CMD ": command not found"
+# define ERR_OPEN ": no such file or directory"
+# define ERR_ACCESS ": permission denied"
+
+int						g_status;
+
+typedef struct s_data	t_data;
+typedef struct s_cmd	t_cmd;
+typedef enum e_builtins
+{
+	E_ECHO = 0,
+	E_CD = 1,
+	E_PWD = 2,
+	E_EXPORT = 3,
+	E_UNSET = 4,
+	E_ENV = 5,
+	E_EXIT = 6
+}	t_builtin;
 
 typedef struct s_cmd
 {
-	char			*cmd;
-	char			**argv;
-	bool			isbuiltin;
-	struct t_cmd	*next;
+	t_data		*ref;
+	char		*cmd;
+	char		**cmd_args;
+	pid_t		pid;
+	int			fd_src;
+	int			fd_dst;
+	int			end[2];
+	bool		isbuiltin;
+	t_builtin	e_builtin;
+	t_cmd		*next;
 }	t_cmd;
 
 typedef struct s_data
 {
 	char			*prompt_line;
 	char			**envp_cp;
-	int				argc;
+	char			**path;
 	size_t			index;
+	t_cmd			*cmd_list;
 }	t_data;
 
 // BUILTINS
