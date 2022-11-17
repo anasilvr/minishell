8f0347b0f6bc5ef8c5e729598732a581ca7522a2
Ana Silveira <anarodri@student.42quebec.com>
2022-11-17 16:43:54 -0500

    Last try.

---
+++ src/pipe_redirect/io.c
@@ -0,0 +1,21 @@
+#include "../../include/minishell.h"
+
+void	setupio(t_data *prog_data)
+{
+	dup2(prog_data->cmd_lst->cmdio_fd[0], 0);
+	dup2(prog_data->cmd_lst->cmdio_fd[1], 1);
+}
+
+void	setup_pipe_in(t_data *prog_data)
+{
+	close(prog_data->pipe_fd[WRITE_ENDPIPE]);
+	dup2(prog_data->pipe_fd[READ_ENDPIPE], 0);
+	close(prog_data->pipe_fd[READ_ENDPIPE]);
+}
+
+void	setup_pipe_out(t_data *prog_data)
+{
+	close(prog_data->pipe_fd[READ_ENDPIPE]);
+	dup2(prog_data->pipe_fd[WRITE_ENDPIPE], 1);
+	close(prog_data->pipe_fd[WRITE_ENDPIPE]);
+}
