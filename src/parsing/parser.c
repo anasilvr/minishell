07b3a2c987bf0fa02574157641c6badbefb13800
Ana Luiza Rodrigues Silveira <anarodri@c2r1p7.42quebec.com>
2022-12-07 15:48:40 -0500

    Setting up parser for open and acess calls before execution.

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -31,9 +31,6 @@ void	split_args(t_cmd *cmd_lst)
 
 void	parser(t_data *data)
 {
-	t_type	*io;
-
-	io = ft_xcalloc(sizeof(t_type));
 	data->cmd_lst = create_cmdlist(data);
 	count_expand(data->cmd_lst, data->token);
 	split_args(data->cmd_lst);
@@ -42,39 +39,4 @@ void	parser(t_data *data)
 	//				is the arg related to a redirection a valid file? (open call)
 	// the access call can be used to set a valid path since you'll have to use access with a path to check its validity.
 	// when access ((path, F_OK | X_OK) == 0)), store this path at data.cmd_lst.path;
-	xfree(io);
 }
-
-
-// void	assign_flags(t_cmd *cmd_lst, t_tok *token, t_type *io, bool *expand)
-// {
-// 	while (cmd_lst && token)
-// 	{
-// 		while (token)
-// 		{
-// 			check_flags(&token->type, io, expand);
-// 			if (*expand)
-// 			{
-// 				cmd_lst->expand += 1;
-// 				*expand = 0;
-// 			}
-// 			if (*io)
-// 			{
-// 				cmd_lst->io_flag = *io;
-// 				*io = 0;
-// 				token = token->next;
-// 				break ;
-// 			}
-// 			token = token->next;
-// 		}
-// 		cmd_lst = cmd_lst->next;
-// 	}
-// }
-
-// void	check_flags(t_type *toktype, t_type *io, bool *expand)
-// {
-// 	if (*toktype >= 2 && *toktype <= 6)
-// 		*io = *toktype;
-// 	if (*toktype == 8 || *toktype == 10)
-// 		*expand = true;
-// }
