6ed73211ed3d1335b204678dbeac4538fc215872
Thomas <tchalifour91@gmail.com>
2022-12-22 15:10:03 -0500

    working on piping_strat

--- .testing/piping_strategy.c
+++ .testing/piping_strategy.c
@@ -5,9 +5,13 @@
 #include <sys/wait.h>
 // #include <wait.h>
 #include <fcntl.h>
+#include <string.h>
 
 void	pipe_loop(void);
 void	pipe_twocmds_demo(char **envp);
+void	ft_call_to_execve(char *cmd_path, char **cmd_argv, char **cmd_envp);
+void	pipe_multicmd_demo(char **envp);
+
 
 int	main(int argc, char **argv, char**envp)
 {
@@ -15,71 +19,134 @@ int	main(int argc, char **argv, char**envp)
 	(void) argv;
 
 	// fork_basic_demo();
-	if (argc <= 3)
-		pipe_twocmds_demo(envp);
-
+	// if (argc <= 3)
+	// pipe_twocmds_demo(envp);
+	// ft_call_to_execve("/bin/ls", argv, envp);
+	pipe_multicmd_demo(envp);
 	return (0);
 }
 
-// static void	pipe_insetup(int *pipe_fd)
-// {
-// 	dup2(pipe_fd[0], 0);
-// 	close(pipe_fd[0]);
-// }
+void	ft_call_to_execve(char *cmd_path, char **cmd_argv, char **cmd_envp)
+{
+	(void)cmd_argv;
 
-// static void	pipe_outsetup(int *pipe_fd)
-// {
-// 	dup2(pipe_fd[1], 1);
-// 	close(pipe_fd[0]);
-// 	close(pipe_fd[1]);
-// }
+	char *argv[2] = {"ls"};
+	if (execve(cmd_path, argv, cmd_envp) == -1)
+	{
+		printf("damn\n");
+		// ft_putstr_fd("Command not found : ", 2);
+		// ft_putstr_fd(cmd_argv[0], 2);
+	}
+}
+
+static void	pipe_insetup(int *pipe_fd)
+{
+	dup2(pipe_fd[0], 0);
+	close(pipe_fd[0]);
+}
+
+static void	pipe_outsetup(int *pipe_fd)
+{
+	dup2(pipe_fd[1], 1);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+}
 // void	fork_loop(int nb_of_cmds)
 // {
 
 // }
 
+void	pipe_multicmd_demo(char **envp)
+{
+	int		i				= 0;
+	char	*cmds_path[]	= {"/bin/ls", "/bin/cat"};
+	char	*cmds_argv1[]	= {"ls", NULL};
+	char	*cmds_argv2[]	= {"grep", "make", NULL};
+	char	*cmds_argv3[]	= {"cat", NULL};
+	char	**cmds_lst[4];
+	// int		fork_pid		= 0;
+	int		pipe_fd[2]		= {-2, -2};
+	int		fork_pid[100]; // Il pourrait etre meiux de prendre le nombre de commandes comme ref
+	int		exit_status;
+
+	cmds_lst[0] = cmds_argv1;
+	cmds_lst[1] = cmds_argv2;
+	cmds_lst[2] = cmds_argv3;
+	cmds_lst[3] = NULL;
+	printf("%s | %s | %s | %s\n", cmds_lst[0], cmds_lst[1], cmds_lst[2], cmds_lst[3]);
+	//Opening all child
+	while (i<= 2)
+	{
+		if (i > 0)
+			pipe_insetup(pipe_fd);
+		if (pipe(pipe_fd) == -1)
+			exit(errno);
+		fork_pid[i] = fork();
+		if (fork_pid[i] == -1)
+			exit(errno);
+		if (fork_pid[i] == 0)//Si le pid est celui du processus enfant
+		{
+			if (i <= 0)
+			{
+				printf("test\n");
+				pipe_outsetup(pipe_fd);
+			}
+			if (execve(cmds_path[i], cmds_lst[i], envp) == -1)
+				perror("");
+		}
+		i++;
+	}
+	while (i >= 0)
+	{
+		waitpid(fork_pid[i], &exit_status, 0);
+		i--;
+	}
+}
+
 
 void	pipe_twocmds_demo(char **envp)
 {
 	int		i				= 0;
-	char	*cmds_path[]	= {"/bin/ls", "/bin/cat", "\0"};
-	char	*cmds_argv1[]	= {"ls"};
-	char	*cmds_argv2[]	= {"cat"};
+	char	*cmds_path[]	= {"/bin/ls", "/bin/cat"};
+	char	*cmds_argv1[]	= {"ls", NULL};
+	char	*cmds_argv2[]	= {"cat", NULL};
 	// char	**cmds_lst[]	= {{"ls"}, {"cat"}};
-	char	**cmds_lst[2];
+	char	**cmds_lst[3];
+
 	int		fork_pid		= 0;
-	// int		pipe_fd[2]		= {-2, -2};
-	// char buf[100001];
-	printf("Executing '%s' and '%s' with a pipe between them.\n", cmds_argv1[0], cmds_argv2[0]);
+	int		pipe_fd[2]		= {-2, -2};
+
 	cmds_lst[0] = cmds_argv1;
 	cmds_lst[1] = cmds_argv2;
-	printf ("%s, %s, %s, %s\n", cmds_lst[0][0], cmds_lst[1][0], cmds_path[0], cmds_path[1]);
-	while (i <= 0)
+	cmds_lst[2] = NULL;
+	while (i <= 1)
 	{
-		// if (i > 0)
-		// {
-		// 	// read(pipe_fd[0], buf, 100);
-		// 	// printf("%s\n", buf);
-		// 	pipe_insetup(pipe_fd);
-		// }
-		// if (pipe(pipe_fd) == -1)
-		// 	exit(errno);
+		if (i > 0)
+		{
+			pipe_insetup(pipe_fd);
+		}
+		if (pipe(pipe_fd) == -1)
+			exit(errno);
 		fork_pid = fork();
 		if (fork_pid == -1)
 			exit(errno);
 		if (fork_pid == 0)//Si le pid est celui du processus enfant
 		{
-			// if (i >= 1)
-			// pipe_outsetup(pipe_fd);
+			if (i <= 0)
+			{
+				printf("test\n");
+				pipe_outsetup(pipe_fd);
+			}
 			if (execve(cmds_path[i], cmds_lst[i], envp) == -1)
-				exit(errno);
+				perror("");
 		}
 		else
 			waitpid(0, NULL, 0);
-			// close(pipe_fd[1]);
+			close(pipe_fd[1]);
 		i++;
 	}
 }
+
 
 
 void	fork_basic_demo(void)
