ed36d30121579e231eda4e197032a4d3b3c1b714
Patarion <62268532+Patarion@users.noreply.github.com>
2023-01-25 16:44:34 -0500

    Update libft.h

--- include/libft/libft.h
+++ include/libft/libft.h
@@ -63,7 +63,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
-void	xfree(void *ptr);
+void	*xfree(void *ptr);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
